26ce3910efd4beb650150b2c64cf7a1194a140f2
Salah Ahmed <riddenariel@gmail.com>
2024-03-03 03:58:32 +0200

    update requests to https

--- script/weatherData.js
+++ script/weatherData.js
@@ -1,6 +1,6 @@
 export async function getWeatherData(query) {
 	const weatherData = await fetch(
-		`http://api.weatherapi.com/v1/current.json?key=95e4d93b6db54c189bc204447240103&q=${query}`,
+		`https://api.weatherapi.com/v1/current.json?key=95e4d93b6db54c189bc204447240103&q=${query}`,
 		{ mode: 'cors' }
 	);
 	const response = await weatherData.json();
